617cdf480e7ed687cc8548a8394763b4d1eb8b65
christinezosche <christine.zosche@gmail.com>
2020-09-27 16:28:28 -0700

    Added User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :first_name, presence: true
+    validates :email, 
+    presence: true, 
+    uniqueness: true,
+    format: { with: /^(.+)@(.+)$/, message: "Invalid email"  }       
+    
+
 
     has_many :trips
     has_many :ratings
