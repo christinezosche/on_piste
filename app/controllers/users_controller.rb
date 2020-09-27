8caa9de103242a3a829775dfde235d72f70721cb
christinezosche <christine.zosche@gmail.com>
2020-09-27 15:43:24 -0700

    Added error messages for failed user signup and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,10 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            @user.save
             session[:user_id] = @user.id
             redirect_to mountains_path
         else
-            redirect_to root_path
+            redirect_to signup_path
         end
     end
 
