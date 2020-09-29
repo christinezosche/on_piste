5032c230ebaf8622de9fca3f76157e1ae87d650e
christinezosche <christine.zosche@gmail.com>
2020-09-29 16:51:23 -0700

    Refactored form to add a new trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -6,11 +6,18 @@ class TripsController < ApplicationController
 
     def create
         @trip = Trip.new(trip_params)
+        @trip.user = current_user
         if @trip.valid?
             @trip.save
             redirect_to mountains_path
         else
             render :new
         end
+    end
+
+    private
+
+    def trip_params
+        params.require(:trip).permit(:date, :mountain)
     end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,7 @@
 class Trip < ApplicationRecord
     belongs_to :user
     belongs_to :mountain
+
+    validates :date, presence: true
+    validates :mountain, presence: true
 end
