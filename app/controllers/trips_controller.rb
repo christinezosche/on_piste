b2ad452661ca9c635c4ebef23d1a7e0bab3d2a6c
christinezosche <christine.zosche@gmail.com>
2020-09-29 16:32:52 -0700

    Added a form to add a new trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,16 @@
 class TripsController < ApplicationController
+
+    def new
+        @trip = Trip.new
+    end
+
+    def create
+        @trip = Trip.new(trip_params)
+        if @trip.valid?
+            @trip.save
+            redirect_to mountains_path
+        else
+            render :new
+        end
+    end
 end
