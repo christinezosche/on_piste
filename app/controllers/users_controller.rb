fc54ce80b4c131fe35ab5ee2f50ad492d12b4f48
christinezosche <christine.zosche@gmail.com>
2020-09-28 21:25:40 -0700

    Added validations and error display messages for User create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,12 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if @user.save
+        if @user.valid?
+            @user.save
             session[:user_id] = @user.id
             redirect_to mountains_path
         else
-            redirect_to signup_path
+            render :new
         end
     end
 
