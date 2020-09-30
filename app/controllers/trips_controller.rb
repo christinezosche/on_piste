172cb097d5cb4b0e09d1e9d63bf978a7e2e9a31a
christinezosche <christine.zosche@gmail.com>
2020-09-30 11:55:48 -0700

    Added nested routes for trips, modified controller actions and trips index page and user show page.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,18 @@
 class TripsController < ApplicationController
 
+    def index
+        if params[:user_id]
+            if User.find_by(id: params[:user_id])
+            @trips = User.find(params[:user_id]).trips
+            else
+            flash[:alert] = "User not found."
+            render :template => "mountains/index"
+            end
+        else
+            redirect_to mountains_path
+        end
+    end
+
     def new
         @trip = Trip.new
     end
@@ -12,7 +25,7 @@ class TripsController < ApplicationController
         @trip = current_user.trips.create(trip_params)
         if @trip.valid?
             @trip.save
-            redirect_to mountains_path
+            redirect_to user_path(current_user)
         else
             render :new
         end
--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,17 @@ Rails.application.routes.draw do
   get "/signup" => "users#new"
   post "/signup" => "users#create"
   post "/logout" => "sessions#destroy"
-  post "/users/:id" => "users#update"
 
   get "/auth/:provider/callback" => "sessions#omniauth"
 
-  resources :ratings, only: [:new, :create]
   resources :trips, only: [:new, :create, :edit, :update, :destroy]
-  resources :mountains, only: [:show, :index]
-  resources :users
+
+  resources :mountains, only: [:show, :index] do
+    resources :trips, only: [:new, :create, :edit, :update, :destroy]
+  end
+
+  resources :users, only: [:new, :create, :edit, :show] do
+    resources :trips, only: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
